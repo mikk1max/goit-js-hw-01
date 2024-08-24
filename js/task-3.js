3f45d14e9a0acb959567e4f407666aad871d7c7c
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-08-24 19:07:01 +0200

    first release

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+function getElementWidth(content, padding, border) {
+    return Number.parseFloat(content) + 2 * (Number.parseFloat(padding) + Number.parseFloat(border));
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
